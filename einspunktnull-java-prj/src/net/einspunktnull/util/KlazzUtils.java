67627da89d257ec759394dd433b7dbd5f185a4e2
Albrecht Nitsche <nitsche@einspunktnull.net>
2013-12-15 18:28:48 +0100

    added refelction styled createInstance mothod to KlazzUtils

--- einspunktnull-java-prj/src/net/einspunktnull/util/KlazzUtils.java
+++ einspunktnull-java-prj/src/net/einspunktnull/util/KlazzUtils.java
@@ -1,5 +1,7 @@
 package net.einspunktnull.util;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.List;
 
@@ -7,6 +9,39 @@ import org.apache.commons.lang3.ClassUtils;
 
 public class KlazzUtils
 {
+
+	public static Object createInstance(Class<?> clazz, Object[] argz)
+	{
+		Constructor<?> constructor = clazz.getConstructors()[0];
+		Class<?>[] parameterTypes = constructor.getParameterTypes();
+		Object[] parameters = new Object[parameterTypes.length];
+
+		int len = parameterTypes.length;
+		for (int i = 0; i < len; i++)
+		{
+			Class<?> paramType = parameterTypes[i];
+			Object arg = argz[i];
+
+			boolean needsToBeCasted = true;
+			if (arg instanceof Long && !paramType.equals(Long.class)) needsToBeCasted = false;
+			else if (arg instanceof Integer && !paramType.equals(Integer.class)) needsToBeCasted = false;
+
+			parameters[i] = needsToBeCasted ? paramType.cast(arg) : arg;
+		}
+
+		Object retObj = null;
+		try
+		{
+			retObj = constructor.newInstance(parameters);
+		}
+		catch (InstantiationException | IllegalAccessException
+				| IllegalArgumentException | InvocationTargetException e)
+		{
+			e.printStackTrace();
+		}
+
+		return retObj;
+	}
 
 	public static Object findByClass(Class<?> clazz, Collection<?> instances)
 	{
