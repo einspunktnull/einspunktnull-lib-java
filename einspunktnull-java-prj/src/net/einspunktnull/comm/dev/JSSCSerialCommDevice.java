10ab000a83fd7d6109c5698cdffeb7b4c3be2301
Albrecht Nitsche <nitsche@einspunktnull.net>
2013-12-28 16:22:09 +0100

    added jssc serial support

---
+++ einspunktnull-java-prj/src/net/einspunktnull/comm/dev/JSSCSerialCommDevice.java
@@ -0,0 +1,142 @@
+package net.einspunktnull.comm.dev;
+
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
+
+public class JSSCSerialCommDevice extends AbstractSerialCommDevice implements SerialPortEventListener
+{
+
+	private SerialPort serialPort;
+
+	public JSSCSerialCommDevice(String name, ICommDeviceListener listener, long baudrate, String portname)
+	{
+		super(name, listener, baudrate, portname);
+	}
+
+	@Override
+	public void start() throws CommDeviceException
+	{
+		serialPort = new SerialPort(portname);
+		try
+		{
+			serialPort.openPort();
+			serialPort.setParams((int) baudrate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+			serialPort.setEventsMask(SerialPort.MASK_RXCHAR);
+			serialPort.addEventListener(this);
+
+		}
+		catch (SerialPortException e)
+		{
+			throw new CommDeviceException("SerialPortException", e);
+		}
+
+	}
+
+	@Override
+	public void serialEvent(SerialPortEvent evt)
+	{
+
+		try
+		{
+			if (evt.isRXCHAR())
+			{
+				while (evt.getEventValue() > 0)
+				{
+					byte[] bytes = serialPort.readBytes(1);
+					byte bite = bytes[0];
+					listener.onByteReceived(JSSCSerialCommDevice.this, bite);
+				}
+			}
+
+		}
+		catch (SerialPortException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void stop() throws CommDeviceException
+	{
+		try
+		{
+			serialPort.removeEventListener();
+			serialPort.closePort();
+		}
+		catch (SerialPortException e)
+		{
+			throw new CommDeviceException("SerialPortException", e);
+		}
+
+	}
+
+	@Override
+	public int available() throws CommDeviceException
+	{
+		throw new CommDeviceException("There is no available()-support for JSSCSerialCommDevice");
+	}
+
+	@Override
+	public int read() throws CommDeviceException
+	{
+		try
+		{
+			return serialPort.readIntArray(1)[0];
+		}
+		catch (SerialPortException e)
+		{
+			throw new CommDeviceException("SerialPortException", e);
+		}
+	}
+
+	@Override
+	public void write(String msg) throws CommDeviceException
+	{
+		try
+		{
+			serialPort.writeString(msg);
+		}
+		catch (SerialPortException e)
+		{
+			throw new CommDeviceException("SerialPortException", e);
+		}
+
+	}
+
+	@Override
+	public void write(byte bite) throws CommDeviceException
+	{
+		try
+		{
+			serialPort.writeByte(bite);
+		}
+		catch (SerialPortException e)
+		{
+			throw new CommDeviceException("SerialPortException", e);
+		}
+
+	}
+
+	@Override
+	public void write(byte[] bytes) throws CommDeviceException
+	{
+		try
+		{
+			serialPort.writeBytes(bytes);
+		}
+		catch (SerialPortException e)
+		{
+			throw new CommDeviceException("SerialPortException", e);
+		}
+
+	}
+
+	@Override
+	public String toString()
+	{
+		return "JSSCSerialCommDevice: " + name + ", " + portname + ", " + baudrate;
+	}
+
+}
